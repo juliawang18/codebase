7875de6edd38f65311661de5848192c0cde10061
Julia Wang <juliawang821@gmail.com>
2019-02-10 21:18:21 -0800

    p5 project

--- p5/p5/empty-example/sketch.js
+++ p5/p5/empty-example/sketch.js
@@ -1,6 +1,7 @@
 let nums = [];
 var count = 0;
 let values = [3, 12,35,23,25,14];
+var lit = 0;
 
 //Making colors!
 var exist_color;
@@ -61,26 +62,44 @@ function mouseClicked() {
         fill(250);
         textSize(18);
         textAlign(CENTER);
-        text("mark first as sorted.", displayWidth/2, 600);
+        text("mark first as sorted", displayWidth/2, 600);
+        lit++;
         return;
     }
 
     //extract first unsorted item
     if(nums[count].status == "unsorted"){
-      console.log("down down");
       nums[count].down();
       nums[count].status = "down";
-
+      drawCommentBox();
+      fill(255);
+      textSize(18);
+      textAlign(CENTER);
+      text("extract the first unsorted element", displayWidth/2, 600);
+      lit++;
       return;
     }
 
     //compare the last sorted and the first unsorted
     if(nums[count].status == "down"){
-      console.log("comparrrreee");
       nums[count].status = "middle"
       nums[count - 1].col = compare_color;
       drawRects();
       drawLabels();
+      drawCommentBox();
+      fill(255);
+      textSize(18);
+      textAlign(CENTER);
+      text("figure where to insert extracted element;",
+        displayWidth/2, 600);
+      text("comparing with sorted number " + nums[count - 1].num,
+        displayWidth/2, 630);
+      lit++;
+      return;
+    }
+
+    if (lit == 21){
+      alert("DONE BOI");
       return;
     }
 
@@ -88,7 +107,6 @@ function mouseClicked() {
     if(nums[count-1].num > nums[count].num){
 
       nums[count].status = "down";
-      console.log("yaaaa");
       nums[count-1].right();
       nums[count].left();
 
@@ -102,20 +120,27 @@ function mouseClicked() {
 
       nums[count].col = sort_color;
 
-      console.log(count);
-      console.log(nums[count]);
-      console.log(nums[count - 1]);
+      drawCommentBox();
+      fill(255);
+      textSize(18);
+      textAlign(CENTER);
+      text(nums[count].num + " > " + nums[count-1].num + " is true, hence move current ",
+        displayWidth/2, 600);
+      text("sorted element to the right by 1.",
+        displayWidth/2, 630);
+
+      lit++;
 
       return;
     }
 
+    //switch pt.2 boiii
     if (nums[count-1].status == "switched" && count >= 2){
       nums[count - 1].col = compare_color;
       drawRects();
       drawLabels();
-    
+
       if(nums[count-1].num < nums[count-2].num){
-        console.log("yoooo");
         nums[count-2].right();
         nums[count-1].left();
 
@@ -125,66 +150,88 @@ function mouseClicked() {
         nums[count-1] = temp;
         count--;
 
-        console.log(count);
-        console.log(nums[count]);
-        console.log(nums[count - 1]);
+        lit++;
 
+        drawCommentBox();
+        fill(255);
+        textSize(18);
+        textAlign(CENTER);
+        text(nums[count].num + " > " + nums[count-1].num + " is true, hence move current",
+          displayWidth/2, 600);
+        text("sorted element to the right by 1.",
+          displayWidth/2, 630);
         return;
 
       } else{
-
+          console.log("yaa");
         nums[count-1].up();
+        drawCommentBox();
+        fill(255);
+        textSize(18);
+        textAlign(CENTER);
+        text(nums[count-2].num + " > " + nums[count-1].num + " is false, insert element at current position",
+          displayWidth/2, 600);
         count++;
-
-        console.log("cooooool");
-        console.log(count);
-        console.log(nums[count]);
-        console.log(nums[count - 1]);
+        lit++;
         return;
       }
     }
 
+    //shove it back up
     if(nums[count-1].status != "switched" && nums[count-1].num <= nums[count].num){
-      console.log("mh")
-      console.log(count);
-      console.log(nums[count]);
-      console.log(nums[count - 1]);
       nums[count].up();
       nums[count-1].col = sort_color;
       drawRects();
       drawLabels();
+
+      drawCommentBox();
+      fill(255);
+      textSize(18);
+      textAlign(CENTER);
+      text(nums[count].num + " < " + nums[count-1].num + " is false, insert element at current position",
+        displayWidth/2, 600);
+
+      lit++;
       count++;
       return;
     }
 
+    //shove it back up pt.2 lol
     if(nums[count].status != "switched" &&count ==1 && nums[count-1].num < nums[count].num){
-      console.log("meh")
-      console.log(count);
-      console.log(nums[count]);
-      console.log(nums[count - 1]);
       nums[count].up();
       nums[count-1].col = sort_color;
       drawRects();
       drawLabels();
+
+      drawCommentBox();
+      fill(255);
+      textSize(18);
+      textAlign(CENTER);
+      text(nums[count].num + " < " + nums[count-1].num + " is false, insert element at current position",
+        displayWidth/2, 600);
+
+      lit++;
       count++;
       return;
     }
 
+    //shove it even more up
     if(count == 1 && nums[count].status == "switched"){
-      console.log("merp")
-      console.log(count);
-      console.log(nums[count]);
-      console.log(nums[count - 1]);
       nums[count-1].up();
       nums[count].col = sort_color;
       nums[count].col = exist_color;
       drawRects();
       drawLabels();
-      count++;
-      return;
-    }
 
-    if (count ==5){
+      drawCommentBox();
+      fill(255);
+      textSize(18);
+      textAlign(CENTER);
+      text(nums[count].num + " < " + nums[count-1].num + " is false, insert element at current position",
+        displayWidth/2, 600);
+
+      lit++;
+      count++;
       return;
     }
   }
@@ -202,7 +249,6 @@ class Thing {
       this.status = "unsorted";
       this.col = color;
       this.xLab = this.xnPos;
-
   }
 
   //moves the bar thingy down
@@ -221,6 +267,7 @@ class Thing {
     this.yPos = this.yPos- this.height - 20;
     this.ynPos = this.ynPos - this.height - 20;
     this.col = sort_color;
+    this.status = "sorted";
     drawRects();
     drawLabels();
   }
@@ -281,13 +328,7 @@ function drawNextButton() {
 
 function drawCommentBox(){
   fill(255, 129, 71);
-  rect(displayWidth/2-200, 570, 400, 80);
-}
-
-function writeComment() {
-  fill(255);
-  textSize(20);
-  textAlign(CENTER);
+  rect(displayWidth/2-250, 570, 500, 80);
 }
 
 function buttonClicked() {
